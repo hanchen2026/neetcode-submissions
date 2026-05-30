2d765052b2e80944f515469572d8fca30c52dd1d
Han Chen <hanc26@gwmail.gwu.edu>
2026-05-29 22:53:46 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,60 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        HashSet<Character> set = new HashSet<>();
+        populate(set);
+
+        while (l <= r) {
+            // skip spaces
+            System.out.println("Comparing: " + (Character.toLowerCase(s.charAt(l))) + " with: " + (Character.toLowerCase(s.charAt(r))));
+
+            if (!set.contains(s.charAt(l)) && !set.contains(s.charAt(r))) {
+                l++;
+                r--;
+                System.out.println("trigger1");
+                continue;
+            }
+
+            if (!set.contains(s.charAt(l))) {
+                l++;
+                System.out.println("trigger2");
+                continue;
+            }
+            else if (!set.contains(s.charAt(r))) {
+                r--;
+                System.out.println("trigger3");
+                continue;
+            }
+            
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) return false;
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    private void populate(HashSet<Character> set) {
+        for (int i = 65; i < (65+27); i++) {
+            set.add((char) i);
+        }
+
+        for (int i = 97; i < (97+27); i++) {
+            set.add((char) i);
+        }
+
+        set.add('0');
+        set.add('1');
+        set.add('2');
+        set.add('3');
+        set.add('4');
+        set.add('5');
+        set.add('6');
+        set.add('7');
+        set.add('8');
+        set.add('9');
+    }
+}
