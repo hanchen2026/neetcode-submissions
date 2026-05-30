1102ee2f8dd5e927bc7d28185254d9a368caa1db
Han Chen <hanc26@gwmail.gwu.edu>
2026-05-29 22:56:38 -0400

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+
+        HashSet<Character> set = new HashSet<>();
+        populate(set);
+
+        while (l <= r) {
+            if (!set.contains(s.charAt(l)) && !set.contains(s.charAt(r))) {
+                l++;
+                r--;
+                continue;
+            }
+            else if (!set.contains(s.charAt(l))) {
+                l++;
+                continue;
+            }
+            else if (!set.contains(s.charAt(r))) {
+                r--;
+                continue;
+            }
+            
+            if (Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))) return false;
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+
+    private void populate(HashSet<Character> set) {
+        for (int i = 65; i < (65+26); i++) {
+            set.add((char) i);
+        }
+
+        for (int i = 97; i < (97+26); i++) {
+            set.add((char) i);
+        }
+
+        set.add('0');
+        set.add('1');
+        set.add('2');
+        set.add('3');
+        set.add('4');
+        set.add('5');
+        set.add('6');
+        set.add('7');
+        set.add('8');
+        set.add('9');
+    }
+}
