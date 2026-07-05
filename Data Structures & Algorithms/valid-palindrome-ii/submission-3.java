7559acb26ca824fb379bec3b0db04293f92b3261
Han Chen <hanc26@gwmail.gwu.edu>
2026-07-05 16:30:26 -0400

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        //instantiate a left and right pointer, as well as an indicator for removed element 
+        int rem = 1;
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l <= r) {
+            if (s.charAt(l) == s.charAt(r)) {       //valid so proceed as usual
+                l++;
+                r--;
+            }
+            else break;
+        }
+
+        if ((verify(s, l, r-1) || (verify(s, l+1, r))) == false) return false;
+        return true;
+    }
+
+    private boolean verify(String s, int l, int r) {
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r)) return false;
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
