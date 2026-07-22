fd0575687448cf595fdc852ecf8d9c9961ca6c85
Han Chen <hanc26@gwmail.gwu.edu>
2026-07-22 00:57:28 -0400

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode ptr = head;
+        ListNode ret = new ListNode();
+
+        while (ptr != null) {
+            ret.val = ptr.val;
+            ptr = ptr.next;
+
+            ListNode temp = new ListNode();
+            temp.next = ret;
+            ret = temp;
+        }
+
+        return ret.next;
+    }
+}
