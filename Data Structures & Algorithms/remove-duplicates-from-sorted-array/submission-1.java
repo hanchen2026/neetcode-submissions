cafcf7cd629dce6c2faedd5ae845c5545952923e
Han Chen <hanc26@gwmail.gwu.edu>
2026-07-22 15:14:32 -0400

    Add: remove-duplicates-from-sorted-array - submission-1

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        //early return condition
+        if (nums.length == 1) return 1; 
+
+        //setting up two pointers
+        int l = 0;
+        int k = 1;
+
+        for(int i = 1; i < nums.length; i++) {
+            //distinct so if directly next do nothing 
+            if (nums[i] > nums[l]) {
+                k++;
+                if (i != l+1) {
+                    int temp = nums[l+1];
+                    nums[l+1] = nums[i];
+                    nums[i] = temp;
+                }
+                l++;
+            }
+            else nums[i] = -111;
+        }
+
+        return k;
+    }
+}
