cb5f48698ffdc861fad4be64e6c56be09ce1481d
Han Chen <hanc26@gwmail.gwu.edu>
2026-07-23 01:26:24 -0400

    Add: search-insert-position - submission-2

---
+++ Data Structures & Algorithms/search-insert-position/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+
+        while (l <= r) {
+            int mid = l + (r-l)/2;
+
+            //handles the case where the index exists in range
+            if (nums[mid] == target) return mid;
+            
+            //left right delta is 1, and does not fit either
+            else if (r-l == 1 && (nums[l] < target && nums[r] > target)) return l+1;
+
+            else if (nums[mid] < target) {
+                l = mid+1;
+            }
+            else {
+                r = mid-1;
+            }
+
+        }
+
+        //not entirely sure why you return l instead of r here, but if it works it works ig?
+                    //since r < l, r is the value directly smaller than the target, 
+                    //which means that l (which is r+1) is the spot where the next target can go.
+        return l;
+    }
+}
