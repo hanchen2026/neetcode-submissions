d293d0212d5c5404ed3d4dfcd27e43e9a01c2149
Han Chen <hanc26@gwmail.gwu.edu>
2026-07-22 20:24:24 -0400

    Add: meeting-schedule - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule/submission-3.java
@@ -0,0 +1,43 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        //sort for consistency (and O(n logn) runtime)
+        Collections.sort(intervals, Comparator.comparingInt(i -> i.start));
+
+        boolean canAttend = true;
+        int l = -1;
+        int r = -1;
+
+        for(Interval i: intervals) {
+            //initializing
+            if (l == -1 && r == -1) {
+                l = i.start;
+                r = i.end;
+            }
+            //if both left then fine
+            else if (i.start < l && i.end <= l) {
+                l = i.start;
+            }
+            //if both right then fine
+            else if (i.start >= r && i.end >= r) {
+                r = i.end;
+            }
+            else {
+                return false;
+            }
+        }
+
+
+        return canAttend;
+    }
+}
