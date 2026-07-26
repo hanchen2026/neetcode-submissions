888d4f818ad94a0f00052b2dbae77bba06f03dbe
Han Chen <hanc26@gwmail.gwu.edu>
2026-07-26 17:55:33 -0400

    Add: roman-to-integer - submission-1

---
+++ Data Structures & Algorithms/roman-to-integer/submission-1.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int romanToInt(String s) {
+
+        //Map of characters in string (you could do like a separate method to save space)
+        Map<Character, Integer> map = Map.of(
+        'I', 1,
+        'V', 5,
+        'X', 10,
+        'L', 50,
+        'C', 100,
+        'D', 500,
+        'M', 1000
+        );
+
+        int total = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            //if at end dont compare and just add value
+            if (i == s.length() - 1) {
+                total += map.get(c);
+            }
+            //look at next character; if legal and makes less than value (ie: IV = 4, V = 5)
+            //subtract current from total, else just add it to total.
+            else if (c == 'I' && (s.charAt(i+1) == 'V' || s.charAt(i+1) == 'X')) {
+                total -= map.get(c);
+            }
+            else if (c == 'X' && (s.charAt(i+1) == 'L' || s.charAt(i+1) == 'C')) {
+                total -= map.get(c);
+            }
+            else if (c == 'C' && (s.charAt(i+1) == 'D' || s.charAt(i+1) == 'M'))  {
+                total -= map.get(c);
+            }
+            else {
+                total += map.get(c);
+            }
+        }
+
+        return total;
+    }
+}
