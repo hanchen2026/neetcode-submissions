dbd84e4b44633c47e84796eb7721df254037d49b
Han Chen <hanc26@gwmail.gwu.edu>
2026-07-27 02:09:17 -0400

    Add: add-binary - submission-4

---
+++ Data Structures & Algorithms/add-binary/submission-4.java
@@ -0,0 +1,69 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+
+        int r = b.length() - 1;
+        int l = a.length() - 1;
+        boolean carry = false;
+
+        while (l >= 0 && r >= 0) {
+            if (a.charAt(l) != b.charAt(r)) {
+                if (carry != true) result.append('1');
+                else {
+                    result.append('0');
+                }
+            }
+            else if (a.charAt(l) == '0') {
+                if (carry != true) result.append('0');
+                else {
+                    result.append('1');
+                    carry = false;
+                }
+            }
+            else {
+                if (carry != true) result.append('0');
+                else result.append('1');
+                carry = true;
+            }
+
+            l--;
+            r--;
+        }
+
+        while (l >= 0) {
+            if (carry == true) {
+                if (a.charAt(l) == '0') {
+                    result.append('1');
+                    carry = false;
+                }
+                else {
+                    result.append('0');
+                }
+            }
+            else {
+                result.append(a.charAt(l));
+            }
+            l--;
+        }
+
+        while (r >= 0) {
+            if (carry == true) {
+                if (b.charAt(r) == '0') {
+                    result.append('1');
+                    carry = false;
+                }
+                else {
+                    result.append('0');
+                }
+            }
+            else {
+                result.append(b.charAt(r));
+            }
+            r--;
+        }
+
+        if (carry) result.append('1');
+
+        return result.reverse().toString();
+    }
+}
