31e1252119432b5787a60875385903e4a4bdc1d4
Han Chen <hanc26@gwmail.gwu.edu>
2026-07-27 02:16:46 -0400

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int reverseBits(int n) {
+
+        //start with 0
+        int result = 0;
+
+        //32 bit solution, so does it 32 times.
+        for (int i = 0; i < 32; i++) {
+
+            //extract next digit for sol
+            result <<= 1;
+
+            // what teh fuck
+            result = result | (n & 1);
+
+            //move to next digit after done
+            n >>= 1;
+        }
+
+        return result;
+    }
+}
