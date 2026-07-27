0f34dcfd73d241c03778d07ff3cfdf85b840f73d
Han Chen <hanc26@gwmail.gwu.edu>
2026-07-26 21:54:00 -0400

    Add: transpose-matrix - submission-0

---
+++ Data Structures & Algorithms/transpose-matrix/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        // m x n matrix = row x col
+        int row = matrix.length;    
+        int col = matrix[0].length;
+
+        //reverse it, mxn ==> nxm
+        int[][] result = new int[col][row];
+
+        for(int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                if (i == j) result[i][j] = matrix[i][j];
+                else {
+                    result[i][j] = matrix[j][i];
+                }
+            }
+        }
+
+        return result;
+    }
+}
