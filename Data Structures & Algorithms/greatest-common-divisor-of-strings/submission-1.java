11a2d167cc04bb2682f0ee29b158231047a87828
Han Chen <hanc26@gwmail.gwu.edu>
2026-07-26 22:53:14 -0400

    Add: greatest-common-divisor-of-strings - submission-1

---
+++ Data Structures & Algorithms/greatest-common-divisor-of-strings/submission-1.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+    
+        // initial gcd calcs O(log(n) time)
+        int a = str1.length();
+        int b = str2.length();
+        int gcd = gcd(a, b);
+
+        // O(N) time
+        String ret = "";
+        for(int i = 0; i < gcd; i++) {
+            if (str1.charAt(i) == str2.charAt(i)) ret += str1.charAt(i);
+            else return "";
+        }
+
+        //O(N) final check
+        String s = (a > b) ? str1 : str2;
+        for(int i = 0; i < a; i++) {
+            if (s.charAt(i) != (ret.charAt( (i % ret.length()) ))) return "";
+        }
+
+        return ret;
+    }
+
+    private int gcd(int a, int b){
+        int bigger = (a > b) ? a : b;
+        int result = (a == bigger) ? b : a;
+        while (result > 0) {
+            int temp = (bigger % result);
+            bigger = result;
+            if (temp == 0) {
+                break;
+            }
+            result = temp;
+        }
+
+        return result;
+    }
+}
