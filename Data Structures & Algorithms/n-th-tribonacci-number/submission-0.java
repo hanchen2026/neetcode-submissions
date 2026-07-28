dabc27c0fac05fae169c5942dfce23f5a91e5f4d
Han Chen <hanc26@gwmail.gwu.edu>
2026-07-28 13:13:50 -0400

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int tribonacci(int n) {
+        int[] cache = new int[n+1];
+        if (n > 0) cache[1] = 1;
+        if (n > 1) cache[2] = 1; 
+
+        return fibRecursive(n, cache);
+    }
+
+    private int fibRecursive(int n, int[] arr) {
+        System.out.println("FASOUKLDhAKSJDH");
+        //if small number return the filled in number
+        if (n < 3) return arr[n];
+                    // System.out.println("N: " + n + " ARR: " + arr[n-1]);
+
+        //if in the cache, return the value instead of fibbing
+        if (arr[n] != 0) return arr[n];
+                    // System.out.println("N2: " + n + " ARR: " + arr[n-1]);
+
+        //else it is not in cache, so perform tribonacci, and then fill in the cache
+        int trib = fibRecursive(n-1, arr) + fibRecursive(n-2, arr) + fibRecursive(n-3, arr);
+        arr[n] = trib;
+        return trib;
+    }
+}
