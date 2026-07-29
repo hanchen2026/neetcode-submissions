adb5e28296d7c63fcc5f3671bb0e3e822db8e6c5
Han Chen <hanc26@gwmail.gwu.edu>
2026-07-28 21:17:50 -0400

    Add: same-binary-tree - submission-6

---
+++ Data Structures & Algorithms/same-binary-tree/submission-6.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        return recursionMethodBleh(p, q);
+    }
+
+    private boolean recursionMethodBleh(TreeNode a, TreeNode b) {
+        if (a == null && b != null || a != null && b == null) return false;
+        else if (a == null && b == null) return true;
+        else if (a.val != b.val) return false;
+        return ((recursionMethodBleh(a.left, b.left)) && (recursionMethodBleh(a.right, b.right)));
+    }
+}
