f00534bb1cc2b0cde43ac0e81f605352c6c0cd15
Han Chen <hanc26@gwmail.gwu.edu>
2026-07-29 02:42:16 -0400

    Add: verifying-an-alien-dictionary - submission-3

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-3.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+
+        //ordering hashmap
+        HashMap<Character, Integer> alphabet = new HashMap<>();
+        for(int i = 0; i < order.length(); i++) {
+            alphabet.put(order.charAt(i), i);
+        }
+
+        //flag for a failure
+        boolean issue = false;
+        for (int i = 0; i < words.length - 1; i++) {
+            //flag for exact match (to be reset every pair config)
+            boolean exactMatch = false;
+
+            //loop through shorter prefix
+            int shorter = (words[i].length() < words[i+1].length()) ? words[i].length() : words[i+1].length();
+
+            for (int j = 0; j < shorter; j++) {
+                char comparatorA = words[i].charAt(j);
+                char comparatorB = words[i+1].charAt(j);
+
+                //if already a fine word (ie: "CAN" and "CRY", move on)
+                if (alphabet.get(comparatorB) > alphabet.get(comparatorA)) break;
+
+                //if at the end, and the last is a match, it is an exact match
+                if (j == shorter - 1 && alphabet.get(comparatorB) == alphabet.get(comparatorA)) exactMatch = true; 
+
+                //else if there is an issue, set a flag (you could break too but im lazy)
+                if (alphabet.get(comparatorB) < alphabet.get(comparatorA)) issue = true;
+            }
+            
+            //if there is an issue, or an exact match with the following word being longer, return false. 
+            if (issue || (exactMatch && words[i].length() > words[i+1].length())) return false;
+        }
+
+        return true;
+    }
+}
