cce4bdafca34aa85f38a9dc80f29fe5497ab65ec
Han Chen <hanc26@gwmail.gwu.edu>
2026-07-31 02:05:09 -0400

    Add: binary-tree-diameter - submission-3

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-3.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    //global diameter parameter to act as global anchor for diameter
+    int diameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        diameterRecursive(root);
+        return diameter;
+    }
+
+    //return depth
+    private int diameterRecursive(TreeNode n) {
+        //if leaf node (or null node), children are empty, 0 depth.
+        if (n == null) return 0;
+
+        //find left subtree max length
+        int l = diameterRecursive(n.left);
+
+        //find right subtree max length
+        int r = diameterRecursive(n.right);
+
+        //return depth of current node 
+        int depth = 1 + Math.max(l, r);
+
+        //calculate diameter at current node (l + r), idk why
+        int currentDiameter = (l + r);
+        
+        //if new diam is better than global, set global to current
+        if (currentDiameter > diameter) diameter = currentDiameter;
+
+        //return current node depth.
+        return depth;
+    }
+}
