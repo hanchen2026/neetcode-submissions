62482ad73bc5567f19819be1aea7033ba1d5c182
Han Chen <hanc26@gwmail.gwu.edu>
2026-08-03 19:37:35 -0400

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        //max for longest consecutive; HashSet for O(1) lookup
+        int max = 0;
+        HashSet<Integer> set = new HashSet<>();
+
+        //populate hashset with integers from the array
+        for (int n: nums) set.add(n);
+
+        //algorithm for thingy
+        for (Integer key: set) {
+            int window = 1;
+            if (set.contains(key-1)) continue;
+            while(true) {
+                if (!set.contains(key+1)) break;
+                window++;
+                key++;
+            }
+            if (window > max) max = window;
+        }
+
+
+        return max;
+    }
+}
